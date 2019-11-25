181544ee6117b99acd7d97807a1bb28745523854
Rishav <rishavjay@hotmail.com>
2019-11-24 21:00:48 -0800

    git commit -m

--- src/sample.js
+++ src/sample.js
@@ -1,15 +1,8 @@
 data = {
   "ast.EXPRESSION" : {
     "@2cb4c3ab" : {
-      "name" : {
-        "1" : "postalCodeEx"
-      },
-      "rules" : {
-        "1" : "ast.RULE@2ef9b8bc",
-        "2" : "ast.RULE@3f2a3a5", 
-        "3" : "ast.RULE@614c5515", 
-        "4" : "ast.RULE@3567135c"
-      }
+      "name" : ["postalCodeEx"],
+      "rules" : ["ast.RULE@2ef9b8bc","ast.RULE@3f2a3a5", "ast.RULE@614c5515", "ast.RULE@3567135c"]
     }
   }
 }
